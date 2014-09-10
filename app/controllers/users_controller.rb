04011f0f817689eae9c9e60c245adc8f6abef3dd
Ryan Troxler <rtroxler@rednovalabs.com>
2014-09-09 22:31:50 -0500

    Finish sign in/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to KiloLeaders!"
-            redirect_to @user
-        else
-          render 'new'
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to KiloLeaders!"
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
